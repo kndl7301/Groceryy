f0f0fb9ad85fd5142d38858357ea9710c65f28b6
unknown <kendalyuce7301@gmail.com>
2025-05-28 15:16:19 +0300

    Prepare for render deployment

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,19 @@ const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 const path = require('path');
 const app = express();
+require('dotenv').config();
+
 
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true
 }));
 
 // MongoDB Atlas connection
-const mongoURI = "mongodb+srv://kendalyuce7301:2019556070@cluster0.r284f.mongodb.net/grocery?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.MONGO_URI;
+
 
 mongoose.connect(mongoURI)
   .then(() => console.log('✅ Connected to MongoDB Atlas'))
@@ -301,13 +304,12 @@ app.post('/api/messages', async (req, res) => {
 
 // === Serve frontend in production ===
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
 
-// === Start Server ===
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
