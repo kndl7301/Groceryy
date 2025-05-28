0d9f0ad5449ce59cb600f4cb016403ff988b56fd
unknown <kendalyuce7301@gmail.com>
2025-05-28 15:25:01 +0300

    Remove node_modules and lock file from backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
 const path = require('path');
 const app = express();
